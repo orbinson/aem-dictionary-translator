8aca2d10f480d31cdc834678ed08b03ade128814
Roy Teeuwen <roy@orbinson.be>
2024-11-14 22:40:49 +0100

    Fix remarks

--- ui.apps/src/main/content/jcr_root/apps/aem-dictionary-translator/clientlibs/dictionary/js/publish.js
+++ ui.apps/src/main/content/jcr_root/apps/aem-dictionary-translator/clientlibs/dictionary/js/publish.js
@@ -4,8 +4,8 @@
     $(window).adaptTo("foundation-registry").register("foundation.collection.action.action", {
         name: "dictionary.publish",
         handler: function (name, el, config, collection, selections) {
-            var message = createEl("div");
-            var intro = createEl("p").appendTo(message);
+            var message = $(document.createElement("div"));
+            var intro = $(document.createElement("p")).appendTo(message);
             if (selections.length === 1) {
                 intro.text(Granite.I18n.get("The item will be published"));
             } else {
@@ -20,22 +20,18 @@
                 primary: true,
                 handler: function () {
                     var agentId = config.data?.agentId || "";
-                    activatePublishPaths(ui, collection, agentId, publishPaths)
+                    activatePublishPaths(ui, collection, agentId, publishPaths);
                 }
             }])
         }
     })
 
     function getPublishPaths(selections) {
-        return selections.flatMap(selection => Array.from(
-            selection.querySelectorAll('[data-publish-path]'))
-            .map(element => element.getAttribute('data-publish-path'))
-            .filter(value => value !== null)
+        return selections.flatMap(selection =>
+            Array.from(selection.querySelectorAll('[data-publish-path]'))
+                .map(element => element.getAttribute('data-publish-path'))
+                .filter(value => value !== null)
         );
-    }
-
-    function createEl(name) {
-        return $(document.createElement(name))
     }
 
     function activatePublishPaths(ui, collection, agentId, paths) {
@@ -54,24 +50,26 @@
             var api = $(collection).adaptTo("foundation-collection");
             if (api && "reload" in api) {
                 api.reload();
-                ui.notify(null, getSuccessMessage(publishPaths));
-                return
+                ui.notify(null, getSuccessMessage(paths));
+                return;
             }
             var contentApi = $(".foundation-content").adaptTo("foundation-content");
-            if (contentApi)
+            if (contentApi) {
                 contentApi.refresh();
-            ui.notify(null, getSuccessMessage(publishPaths))
+            }
+            ui.notify(null, getSuccessMessage(paths));
         }).fail(function (xhr) {
             var title = Granite.I18n.get("Error");
             var message = Granite.I18n.getVar($(xhr.responseText).find("#Message").html());
-            ui.alert(title, message, "error")
+            ui.alert(title, message, "error");
         })
     }
 
     function getSuccessMessage(paths) {
         var successMessage = Granite.I18n.get("{0} items have been published", paths.length);
-        if (paths.length === 1)
+        if (paths.length === 1) {
             successMessage = Granite.I18n.get("The item has been published");
-        return successMessage
+        }
+        return successMessage;
     }
 })();
