40dfa62d3956f6a742b0ab1f5f0ae5d1ff61a3be
Barry d'Hoine <barry.dhoine@telenetgroup.be>
2024-11-29 11:17:06 +0100

    Avoid using foundation prefix

--- ui.apps/src/main/content/jcr_root/apps/aem-dictionary-translator/clientlibs/dictionary/js/activecondition.js
+++ ui.apps/src/main/content/jcr_root/apps/aem-dictionary-translator/clientlibs/dictionary/js/activecondition.js
@@ -2,14 +2,14 @@
     $(window).adaptTo("foundation-registry").register("foundation.collection.action.activecondition", {
         name: "aem-dictionary-translator.dictionary.editable",
         handler: function (name, el, config, collection, selections) {
-            return $(selections[0]).data("foundation-collection-item-editable");
+            return $(selections[0]).data("dictionary-item-editable");
         }
     });
 
     $(window).adaptTo("foundation-registry").register("foundation.collection.action.activecondition", {
         name: "aem-dictionary-translator.dictionary.readonly",
         handler: function (name, el, config, collection, selections) {
-            return !$(selections[0]).data("foundation-collection-item-editable");
+            return !$(selections[0]).data("dictionary-item-editable");
         }
     });
 })();
