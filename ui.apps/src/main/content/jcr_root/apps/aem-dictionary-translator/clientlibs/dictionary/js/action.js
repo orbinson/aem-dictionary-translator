e9bf1bc908612d6f6187c5c7f989ea652f80baa9
Barry d'Hoine <barrydhoine@gmail.com>
2024-11-30 21:07:00 +0100

    Cleanup javascript for replication

--- ui.apps/src/main/content/jcr_root/apps/aem-dictionary-translator/clientlibs/dictionary/js/action.js
+++ ui.apps/src/main/content/jcr_root/apps/aem-dictionary-translator/clientlibs/dictionary/js/action.js
@@ -1,42 +1,38 @@
 (function () {
-    let REPLICATE_URL = Granite.HTTP.externalize("/bin/replicate");
-
     $(window).adaptTo("foundation-registry").register("foundation.collection.action.action", {
         name: "aem-dictionary-translator.dictionary.publish",
         handler: function (name, el, config, collection, selections) {
-            let message = $(document.createElement("div"));
-            let intro = $(document.createElement("p")).appendTo(message);
-            if (selections.length === 1) {
-                intro.text(Granite.I18n.get("The item will be published"));
-            } else {
-                intro.text(Granite.I18n.get("{0} items will be published.", selections.length));
-            }
-            let publishPaths = getPublishPaths(selections);
             let ui = $(window).adaptTo("foundation-ui");
-            ui.prompt("Publish", message.html(), "notice", [{
-                text: "cancel"
-            }, {
-                text: "publish",
-                primary: true,
-                handler: function () {
-                    let agentId = config.data?.agentId || "";
-                    activatePublishPaths(ui, collection, agentId, publishPaths);
+            let title = Granite.I18n.get("Publish dictionary");
+            let message = Granite.I18n.get(`{0} item${selections.length > 1 ? 's' : ''} will be published.`, selections.length);
+
+            ui.prompt(title, message, "notice", [
+                {
+                    text: Granite.I18n.get("Cancel")
+                },
+                {
+                    text: Granite.I18n.get("Publish"),
+                    primary: true,
+                    handler: function () {
+                        let agentId = config.data?.agentId || "";
+                        replicate(ui, collection, agentId, getPublishPaths(selections));
+                    }
                 }
-            }])
+            ]);
         }
     })
 
     function getPublishPaths(selections) {
         return selections.flatMap(selection =>
-            Array.from(selection.querySelectorAll('[data-publish-path]'))
-                .map(element => element.getAttribute('data-publish-path'))
+            Array.from(selection.querySelectorAll("[data-publish-path]"))
+                .map(element => element.getAttribute("data-publish-path"))
                 .filter(value => value !== null)
         );
     }
 
-    function activatePublishPaths(ui, collection, agentId, paths) {
+    function replicate(ui, collection, agentId, paths) {
         $.ajax({
-            url: REPLICATE_URL,
+            url: Granite.HTTP.externalize("/bin/replicate"),
             type: "POST",
             data: {
                 _charset_: "utf-8",
@@ -47,30 +43,15 @@
         }).always(function () {
             ui.clearWait();
         }).done(function () {
-            let api = $(collection).adaptTo("foundation-collection");
-            if (api && "reload" in api) {
-                // TODO: Throwing exception in 6.5
-                // api.reload();
-                ui.notify(null, getSuccessMessage(paths));
-                return;
-            }
             let contentApi = $(".foundation-content").adaptTo("foundation-content");
             if (contentApi) {
                 contentApi.refresh();
             }
-            ui.notify(null, getSuccessMessage(paths));
+            ui.notify(null, Granite.I18n.get(`{0} item${paths.length > 1 ? 's' : ''} have been published`, paths.length), "success");
         }).fail(function (xhr) {
-            let title = Granite.I18n.get("Error");
+            let title = Granite.I18n.get("Publication of dictionary failed");
             let message = Granite.I18n.getVar($(xhr.responseText).find("#Message").html());
             ui.alert(title, message, "error");
         })
-    }
-
-    function getSuccessMessage(paths) {
-        let successMessage = Granite.I18n.get("{0} items have been published", paths.length);
-        if (paths.length === 1) {
-            successMessage = Granite.I18n.get("The item has been published");
-        }
-        return successMessage;
     }
 })();
