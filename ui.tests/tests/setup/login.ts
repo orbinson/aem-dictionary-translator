08f18ddff4d58a8154caa1f106c762cd7010f887
Barry d'Hoine <barrydhoine@gmail.com>
2024-12-10 01:01:07 +0100

    Update dependencies and increate timeout for login

--- ui.tests/tests/setup/login.ts
+++ ui.tests/tests/setup/login.ts
@@ -4,6 +4,9 @@ import * as path from "path";
 const authFile = path.join(__dirname, "../../playwright/.auth/user.json");
 
 setup("Login to session", async ({ page, httpCredentials }) => {
+    // increase timeout for initial login
+    setup.setTimeout(60 * 1000);
+
     await page.goto("/");
 
     await page.getByPlaceholder("User name").fill(httpCredentials.username);
