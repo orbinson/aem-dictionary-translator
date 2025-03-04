f22ee3f311efe9071b93dde670e3fd59063cdcdd
Barry d'Hoine <barrydhoine@gmail.com>
2025-03-04 09:31:59 +0100

    Fix replication

--- ui.apps/src/main/content/jcr_root/apps/aem-dictionary-translator/clientlibs/dictionary/js/action.js
+++ ui.apps/src/main/content/jcr_root/apps/aem-dictionary-translator/clientlibs/dictionary/js/action.js
@@ -1,57 +1,104 @@
 (function () {
     $(window).adaptTo("foundation-registry").register("foundation.collection.action.action", {
         name: "aem-dictionary-translator.dictionary.publish",
-        handler: function (name, el, config, collection, selections) {
-            let ui = $(window).adaptTo("foundation-ui");
-            let title = Granite.I18n.get("Publish dictionary");
-            let message = Granite.I18n.get(`{0} item${selections.length > 1 ? 's' : ''} will be published.`, selections.length);
+        handler: (name, element, config, collection, selections) => {
+            const agentId = config.data?.agentId || "publish";
+            const paths = selections
+                .map(selection => selection.dataset.dictionaryPublishPaths.split(","))
+                .flat();
+            const recursive = selections[0].dataset.dictionaryPublishRecursive === "true";
+            publishPaths(agentId, paths, selections.length, recursive);
+        }
+    });
 
-            ui.prompt(title, message, "notice", [
-                {
-                    text: Granite.I18n.get("Cancel")
-                },
-                {
-                    text: Granite.I18n.get("Publish"),
-                    primary: true,
-                    handler: function () {
-                        let agentId = config.data?.agentId || "";
-                        replicate(ui, collection, agentId, getPublishPaths(selections));
+    async function publishPaths(agentId, paths, size, recursive) {
+        const ui = $(window).adaptTo("foundation-ui");
+        const title = Granite.I18n.get("Publish dictionary");
+        const message = Granite.I18n.get(`{0} item${size > 1 ? 's' : ''} will be published.`, size);
+
+        ui.prompt(title, message, "notice", [
+            {
+                text: Granite.I18n.get("Publish"),
+                primary: true,
+                handler: async () => {
+                    try {
+                        if (recursive) {
+                            await Promise.all(await replicateRecursive(agentId, paths[0]));
+                        } else {
+                            await Promise.all(await replicateBatched(agentId, paths));
+                        }
+                        ui.notify(null, Granite.I18n.get("Publication succeeded."), "success");
+                    } catch (error) {
+                        const title = Granite.I18n.get("Publication failed");
+                        ui.alert(title, error.message, "error");
                     }
                 }
-            ]);
-        }
-    })
-
-    function getPublishPaths(selections) {
-        return selections.flatMap(selection =>
-            Array.from(selection.querySelectorAll("[data-publish-path]"))
-                .map(element => element.getAttribute("data-publish-path"))
-                .filter(value => value !== null)
-        );
+            },
+            {text: Granite.I18n.get("Cancel")},
+        ]);
     }
 
-    function replicate(ui, collection, agentId, paths) {
-        $.ajax({
-            url: Granite.HTTP.externalize("/bin/replicate"),
-            type: "POST",
-            data: {
-                _charset_: "utf-8",
-                cmd: "Activate",
-                path: paths,
-                agentId: agentId || "publish"
-            }
-        }).always(function () {
-            ui.clearWait();
-        }).done(function () {
-            let contentApi = $(".foundation-content").adaptTo("foundation-content");
-            if (contentApi) {
-                contentApi.refresh();
-            }
-            ui.notify(null, Granite.I18n.get(`{0} item${paths.length > 1 ? 's' : ''} have been published`, paths.length), "success");
-        }).fail(function (xhr) {
-            let title = Granite.I18n.get("Publication of dictionary failed");
-            let message = Granite.I18n.getVar($(xhr.responseText).find("#Message").html());
-            ui.alert(title, message, "error");
-        })
+    function query(path, limit, offset) {
+        const params = new URLSearchParams({
+            "path": path,
+            "p.limit": limit,
+            "p.offset": offset,
+            "p.hits": "selective",
+            "p.properties": "jcr:path",
+        });
+        const endpoint = Granite.HTTP.externalize("/bin/querybuilder.json");
+
+        return fetch(`${endpoint}?${params.toString()}`)
+            .then(response => response.json());
+    }
+
+    async function replicateRecursive(agentId, path, batchSize = 100) {
+        let offset = 0;
+        let total = null;
+        let replications = [];
+
+        do {
+            const response = await query(path, batchSize, offset);
+            total = total ?? response.total;
+
+            const paths = response.hits.map(hit => hit["jcr:path"]);
+            replications.push(replicate(agentId, paths));
+
+            offset += batchSize;
+        } while (offset < total);
+
+        return replications;
+    }
+
+    async function replicateBatched(agentId, paths, batchSize = 100) {
+        const replications = [];
+
+        for (let i = 0; i < paths.length; i += batchSize) {
+            const batch = paths.slice(i, i + batchSize);
+            replications.push(replicate(agentId, batch));
+        }
+
+        return replications;
+    }
+
+    function replicate(agentId, paths) {
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: Granite.HTTP.externalize("/bin/replicate"),
+                type: "POST",
+                data: {
+                    _charset_: "utf-8",
+                    cmd: "Activate",
+                    path: paths,
+                    agentId: agentId,
+                    batch: "true"
+                }
+            }).done((data) => {
+                resolve();
+            }).fail(xhr => {
+                const message = $(xhr.responseText).find("#Message").html() || "Failed to publish dictionary.";
+                reject(new Error(message));
+            });
+        });
     }
 })();
