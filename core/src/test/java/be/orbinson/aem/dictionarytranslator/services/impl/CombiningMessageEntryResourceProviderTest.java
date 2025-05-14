orbinson/aem-dictionary-translator

commit e78538d48e748dc0aba8b363a49a1a5340e60891

Support JSON-file based dictionaries in read-only mode
Clean up exception handling and improve low-level API exposed from
DictionaryService.
Caching added to DictionaryService

This closes #26

core/src/main/java/be/orbinson/aem/dictionarytranslator/services/DictionaryService.java
@@ -1,43 +1,169 @@
 package be.orbinson.aem.dictionarytranslator.services;
 
-import be.orbinson.aem.dictionarytranslator.exception.DictionaryException;
-import com.day.cq.replication.ReplicationException;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
 import org.apache.sling.api.resource.PersistenceException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
+import org.osgi.annotation.versioning.ProviderType;
 
-import javax.jcr.RepositoryException;
-import java.util.List;
+import com.day.cq.replication.ReplicationException;
 
+import be.orbinson.aem.dictionarytranslator.exception.DictionaryException;
+
+/**
+ * Low-level service interface for reading and creating/updating/deleting dictionaries in AEM.
+ * <p>
+ * A high-level read-only API is provided by the {@link be.orbinson.aem.dictionarytranslator.models.Dictionary} model and by the {@link be.orbinson.aem.dictionarytranslator.services.impl.CombiningMessageEntryResourceProvider}.
+ *
+ */
+@ProviderType
 public interface DictionaryService {
 
-    boolean isEditableDictionary(Resource resource);
+    boolean isEditableDictionary(Resource dictionaryResource);
 
     List<Resource> getDictionaries(ResourceResolver resourceResolver);
 
     void createDictionary(Resource parent, String name, String[] languages, String basename) throws PersistenceException;
 
-    void deleteDictionary(ResourceResolver resourceResolver, String dictionaryPath) throws DictionaryException;
+    void deleteDictionary(ResourceResolver resourceResolver, String dictionaryPath) throws DictionaryException, ReplicationException, PersistenceException;
 
     List<String> getLanguages(Resource dictionaryResource);
 
-    void deleteLanguage(ResourceResolver resourceResolver, Resource dictionaryResource, String language) throws DictionaryException;
+    void deleteLanguage(Resource dictionaryResource, String language) throws DictionaryException, ReplicationException, PersistenceException;
 
     void addLanguage(Resource dictionaryResource, String language, String basename) throws PersistenceException;
 
-    Resource getLanguageResource(Resource dictionaryResource, String language);
-
     String getBasename(Resource dictionaryResource);
 
-    List<String> getKeys(Resource dictionaryResource);
+    List<String> getKeys(Resource dictionaryResource) throws DictionaryException;
 
-    boolean keyExists(Resource dictionaryResource, String language, String key);
+    /**
+     * Returns the message entry for the given language and key.
+     * @param dictionaryResource
+     * @param language
+     * @param key
+     * @return {@code true} if the message entry exists, {@code false} otherwise
+     * @throws DictionaryException if either language or key does not exist below the given dictionary resource
+     */
+    boolean keyExists(Resource dictionaryResource, String language, String key) throws DictionaryException;
 
-    Resource getMessageEntryResource(Resource languageResource, String key);
+    /**
+     * Either creates a new message entry or updates an existing one. The changes are not persisted until the resource resolver is committed.
+     * @param dictionaryResource
+     * @param language
+     * @param key
+     * @param message
+     * @throws PersistenceException in case creating a new resource failed
+     * @throws DictionaryException in case the language doees not exist below the given dictionary resource
+     */
+    void createOrUpdateMessageEntry(Resource dictionaryResource, String language, String key, String message) throws PersistenceException, DictionaryException;
 
-    void createMessageEntry(ResourceResolver resourceResolver, Resource dictionaryResource, String language, String key, String message) throws PersistenceException;
+    /**
+     * Deletes the message entry for the given language and key. The changes are not persisted until the resource resolver is committed.
+     * However the message entry is immediately scheduled for deactivation.
+     * @param dictionaryResource
+     * @param language
+     * @param key
+     * @throws PersistenceException in case deleting the resource failed
+     * @throws ReplicationException in case deactivation failed
+     * @throws DictionaryException if either language or key does not exist below the given dictionary resource
+     */
+    void deleteMessageEntry(Resource dictionaryResource, String language, String key)  throws PersistenceException, ReplicationException, DictionaryException;
 
-    void updateMessageEntry(ResourceResolver resourceResolver, Resource dictionaryResource, String language, String key, String message) throws PersistenceException, RepositoryException;
+    /**
+     * Returns all message entries for the given language below the given dictionary resource.
+     * @param dictionaryResource
+     * @param language
+     * @return all message entries for the given language. The key of the map is the key of the message entry and the value is a {@link Message} object containing the actual text and other metadata.
+     * @throws DictionaryException if the dictionary for the given language does not exist
+     */
+    Map<String, Message> getMessages(Resource dictionaryResource, String language) throws DictionaryException;
 
-    void deleteMessageEntry(ResourceResolver resourceResolver, Resource combiningMessageEntryResource) throws PersistenceException, ReplicationException;
+    /** 
+     * Encapsulating a single message entry (for one language).
+     * Exposes both the actual text as well as the resource path of the source message entry.
+     * The latter is only available if the message is based on a single resource (and not based on a JSON file).
+     */
+    public final class Message {
+        private final String text;
+        private final String resourcePath;
+
+        public Message(String message, String resourcePath) {
+            this.text = message;
+            this.resourcePath = resourcePath;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public Optional<String> getResourcePath() {
+            return Optional.ofNullable(resourcePath);
+        }
+
+        @Override
+        public String toString() {
+            return "Message [text=" + text + ", resourcePath=" + resourcePath + "]";
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(text, resourcePath);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Message other = (Message) obj;
+            return Objects.equals(text, other.text) && Objects.equals(resourcePath, other.resourcePath);
+        }
+    }
+
+    enum DictionaryType {
+        SLING_MESSAGE_ENTRY("sling:MessageEntry"),
+        JSON_FILE("JSON file"),
+        /** the languages below this dictionary have different types, only applicable on dictionary level, not on language level */
+        MIXED("Mixed");
+
+        private final String label;
+
+        DictionaryType(String label) {
+            this.label = label;
+        }
+
+        @Override
+        public String toString() {
+            return label;
+        }
+    }
+
+    /**
+     * Returns the type of the dictionary for the given language.
+     * @param dictionaryResource
+     * @param language
+     * @return the type of the dictionary for the given language
+     * @throws DictionaryException if the dictionary for the given language does not exist
+     */
+    DictionaryType getType(Resource dictionaryResource, String language) throws DictionaryException;
+
+    /**
+     * Returns the type of the dictionary (actually the derived type from the type of all languages).
+     * <p>
+     * This method will return {@link DictionaryType#MIXED} if the dictionary contains languages of different types and 
+     * {@link DictionaryType#SLING_MESSAGE_ENTRY} if the dictionary doesn't contain any languages yet.
+     *
+     * @param dictionaryResource the dictionary resource
+     * @return the derived type of the dictionary
+     */
+    DictionaryType getType(Resource dictionaryResource);
+
 }

core/src/main/java/be/orbinson/aem/dictionarytranslator/services/impl/CombiningMessageEntryResourceProvider.java
@@ -1,10 +1,16 @@
 package be.orbinson.aem.dictionarytranslator.services.impl;
 
-import be.orbinson.aem.dictionarytranslator.services.DictionaryService;
-import be.orbinson.aem.dictionarytranslator.utils.DictionaryConstants;
-import com.adobe.granite.ui.components.ds.ValueMapResource;
-import com.day.text.Text;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.jackrabbit.util.Text;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceNotFoundException;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.wrappers.ValueMapDecorator;
 import org.apache.sling.spi.resource.provider.ResolveContext;
@@ -15,8 +21,21 @@ import org.jetbrains.annotations.Nullable;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
-import java.util.*;
+import com.adobe.granite.ui.components.ds.ValueMapResource;
 
+import be.orbinson.aem.dictionarytranslator.exception.DictionaryException;
+import be.orbinson.aem.dictionarytranslator.services.DictionaryService;
+import be.orbinson.aem.dictionarytranslator.services.DictionaryService.Message;
+
+/**
+ * Resource provider exposing all translations for one dictionary entry.
+ * It expects the path to be of the form:
+ * {@code /mnt/dictionary/<dictionaryPath>/<key>}
+ * <p>
+ * The key is unescaped with {@link Text#unescapeIllegalJcrChars(String)} to also allow "/" in it.
+ * This resource provider is used to combine message entries from different languages into a single resource.
+ * It is used in the AEM UI to display the combined message entries for a given key.
+ */
 @Component(
         service = ResourceProvider.class,
         property = {
@@ -37,28 +56,6 @@ public class CombiningMessageEntryResourceProvider extends ResourceProvider<Obje
     @Reference
     private DictionaryService dictionaryService;
 
-    private Map<String, Object> getValuesAndMessageEntryPaths(Resource dictionaryResource, String key, List<String> languages) {
-        Map<String, Object> properties = new HashMap<>();
-        List<String> messageEntryPaths = new ArrayList<>();
-
-        for (String language : languages) {
-            Resource languageResource = dictionaryService.getLanguageResource(dictionaryResource, language);
-            if (languageResource != null) {
-                Resource messageEntryResource = dictionaryService.getMessageEntryResource(languageResource, key);
-                if (messageEntryResource != null) {
-                    properties.put(language, messageEntryResource.getValueMap().get(DictionaryConstants.SLING_MESSAGE, ""));
-                    messageEntryPaths.add(messageEntryResource.getPath());
-                } else {
-                    properties.put(language, "");
-                }
-            }
-        }
-
-        properties.put(MESSAGE_ENTRY_PATHS, messageEntryPaths);
-
-        return properties;
-    }
-
     @Override
     public @Nullable Resource getResource(@NotNull ResolveContext<Object> ctx, @NotNull String path, @NotNull ResourceContext resourceContext, @Nullable Resource parent) {
         ResourceResolver resourceResolver = ctx.getResourceResolver();
@@ -67,45 +64,58 @@ public class CombiningMessageEntryResourceProvider extends ResourceProvider<Obje
             return null;
         }
 
-        String key = Text.getName(path);
-        String dictionaryPath = Text.getRelativeParent(path, 1).replaceFirst(ROOT, "");
+        String key = extractKeyFromPath(path);
+        String dictionaryPath = getDictionaryPath(path);
         Resource dictionaryResource = resourceResolver.getResource(dictionaryPath);
 
-        if (dictionaryResource != null && isMessageEntry(dictionaryResource, key)) {
-            Map<String, Object> properties = new HashMap<>();
-            properties.put(KEY, key);
-            properties.put("path", path);
-            properties.put("editable", dictionaryService.isEditableDictionary(dictionaryResource));
-            properties.put(DICTIONARY_PATH, dictionaryPath);
+        if (dictionaryResource != null) {
+            @NotNull Map<String, Message> messagePerLanguage = new HashMap<>();
             List<String> languages = dictionaryService.getLanguages(dictionaryResource);
-            properties.put(LANGUAGES, languages);
-            properties.putAll(getValuesAndMessageEntryPaths(dictionaryResource, key, languages));
-            return new ValueMapResource(resourceResolver, path, RESOURCE_TYPE, new ValueMapDecorator(properties));
+            for (String language : languages) {
+                Message message;
+                try {
+                    message = dictionaryService.getMessages(dictionaryResource, language).get(key);
+                } catch (DictionaryException e) {
+                    throw new ResourceNotFoundException("Unable to get message entries for language '" + language + "' in dictionary '" + dictionaryPath + "'", e);
+                }
+                messagePerLanguage.put(language, message);
+            }
+           return new ValueMapResource(resourceResolver, path, RESOURCE_TYPE, new ValueMapDecorator(createResourceProperties(path, dictionaryService.isEditableDictionary(dictionaryResource), messagePerLanguage)));
+        } else {
+            throw new ResourceNotFoundException(path, "Unable to get underlying dictionary resource for path '" + dictionaryPath + "'");
         }
-        return null;
     }
 
-    private boolean isMessageEntry(Resource dictionaryResource, String key) {
-        List<String> languages = dictionaryService.getLanguages(dictionaryResource);
-        // in order to speed things up always start with language "en" (if existing)
-        String mostCompleteLanguage = Locale.ENGLISH.getLanguage();
-        if (languages.contains(mostCompleteLanguage)) {
-            languages.remove(mostCompleteLanguage);
-            languages.add(0, mostCompleteLanguage);
-        }
+    private static String extractKeyFromPath(@NotNull String path) {
+        return Text.unescapeIllegalJcrChars(Text.getName(path));
+    }
 
-        if (!languages.isEmpty()) {
-            for (String language : languages) {
-                Resource languageResource = dictionaryService.getLanguageResource(dictionaryResource, language);
-                if (languageResource != null) {
-                    Resource messageEntryResource = dictionaryService.getMessageEntryResource(languageResource, key);
-                    if (messageEntryResource != null) {
-                        return true;
-                    }
-                }
+    public static @NotNull Map<String, Object> createResourceProperties(@NotNull String path, boolean isEditable, @NotNull Map<String, Message> messagePerLanguage) {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put(KEY, extractKeyFromPath(path));
+        properties.put("path", path);
+        properties.put("editable", isEditable);
+        properties.put(DICTIONARY_PATH, getDictionaryPath(path));
+        properties.put(LANGUAGES, messagePerLanguage.keySet());
+        List<String> messageEntryPaths = new ArrayList<>();
+        for (Entry<String, Message> messageEntryPerLanguageEntry : messagePerLanguage.entrySet()) {
+            Message message = messageEntryPerLanguageEntry.getValue();
+            final String text;
+            if (message == null) {
+                text = "";
+            } else {
+                text = message.getText();
+                message.getResourcePath().ifPresent(messageEntryPaths::add);
             }
+            properties.put(messageEntryPerLanguageEntry.getKey(), text);
         }
-        return false;
+        // all paths to replicate
+        properties.put(MESSAGE_ENTRY_PATHS, messageEntryPaths);
+        return properties;
+    }
+
+    public static @NotNull String getDictionaryPath(@NotNull String path) {
+        return Text.getRelativeParent(path, 1).replaceFirst(ROOT, "");
     }
 
     @Override

core/src/main/java/be/orbinson/aem/dictionarytranslator/models/Dictionary.java
@@ -1,9 +1,14 @@
 package be.orbinson.aem.dictionarytranslator.models;
 
-import org.apache.sling.api.resource.Resource;
-
 import java.util.List;
 
+import org.apache.sling.api.resource.Resource;
+import org.osgi.annotation.versioning.ProviderType;
+
+import be.orbinson.aem.dictionarytranslator.exception.DictionaryException;
+import be.orbinson.aem.dictionarytranslator.services.DictionaryService;
+
+@ProviderType
 public interface Dictionary {
     Resource getResource();
 
@@ -13,9 +18,11 @@ public interface Dictionary {
 
     List<String> getLanguages();
 
-    List<String> getKeys();
+    List<String> getKeys() throws DictionaryException;
 
     boolean isEditable();
 
-    int getKeyCount();
+    int getKeyCount() throws DictionaryException;
+    
+    DictionaryService.DictionaryType getType();
 }

core/src/main/java/be/orbinson/aem/dictionarytranslator/models/impl/DictionaryImpl.java
@@ -1,7 +1,10 @@
 package be.orbinson.aem.dictionarytranslator.models.impl;
 
+import be.orbinson.aem.dictionarytranslator.exception.DictionaryException;
 import be.orbinson.aem.dictionarytranslator.models.Dictionary;
 import be.orbinson.aem.dictionarytranslator.services.DictionaryService;
+import be.orbinson.aem.dictionarytranslator.services.DictionaryService.DictionaryType;
+
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
@@ -48,12 +51,18 @@ public class DictionaryImpl implements Dictionary {
     }
 
     @Override
-    public int getKeyCount() {
+    public int getKeyCount() throws DictionaryException {
         return getKeys().size();
     }
 
     @Override
-    public List<String> getKeys() {
+    public List<String> getKeys() throws DictionaryException {
         return dictionaryService.getKeys(resource);
     }
+
+    @Override
+    public DictionaryType getType() {
+        return dictionaryService.getType(resource);
+    }
+
 }

core/src/main/java/be/orbinson/aem/dictionarytranslator/models/package-info.java
@@ -1,4 +1,4 @@
-@Version("2.0.0")
+@Version("3.0.0")
 package be.orbinson.aem.dictionarytranslator.models;
 
 import org.osgi.annotation.versioning.Version;

core/src/main/java/be/orbinson/aem/dictionarytranslator/services/package-info.java
@@ -1,4 +1,4 @@
-@Version("1.0.0")
+@Version("2.0.0")
 package be.orbinson.aem.dictionarytranslator.services;
 
 import org.osgi.annotation.versioning.Version;

core/src/main/java/be/orbinson/aem/dictionarytranslator/servlets/action/CreateMessageEntryServlet.java
@@ -1,5 +1,6 @@
 package be.orbinson.aem.dictionarytranslator.servlets.action;
 
+import be.orbinson.aem.dictionarytranslator.exception.DictionaryException;
 import be.orbinson.aem.dictionarytranslator.services.DictionaryService;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
@@ -57,19 +58,20 @@ public class CreateMessageEntryServlet extends SlingAllMethodsServlet {
                     LOG.debug("Create message entry on path '{}/{}'", dictionary, key);
                     String message = request.getParameter(language);
                     if (!dictionaryService.keyExists(dictionaryResource, language, key)) {
-                        dictionaryService.createMessageEntry(resourceResolver, dictionaryResource, language, key, message);
+                        dictionaryService.createOrUpdateMessageEntry(dictionaryResource, language, key, message);
                     } else {
                         HtmlResponse htmlResponse = new HtmlResponse();
                         htmlResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST, String.format("Can not create message entry %s, key already exists", key));
                         htmlResponse.send(response, true);
                     }
                 }
+                resourceResolver.commit();
             } else {
                 HtmlResponse htmlResponse = new HtmlResponse();
                 htmlResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST, String.format("Unable to get dictionary '%s'", dictionary));
                 htmlResponse.send(response, true);
             }
-        } catch (PersistenceException e) {
+        } catch (PersistenceException|DictionaryException e) {
             HtmlResponse htmlResponse = new HtmlResponse();
             htmlResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, String.format("Unable to create key '%s' on dictionary '%s': %s", key, dictionary, e.getMessage()));
             htmlResponse.send(response, true);

commit 263cbf3f00167dbff14ff86a1ba000c55f961d2e

Fix sorting order of values in "languages" property of CombiningMessageEntryResource
This restores using the alphabetical order in the message entry table
rows. This regressed with
https://github.com/orbinson/aem-dictionary-translator/commit/e78538d48e748dc0aba8b363a49a1a5340e60891

core/src/main/java/be/orbinson/aem/dictionarytranslator/services/DictionaryService.java
@@ -31,6 +31,11 @@ public interface DictionaryService {
 
     void deleteDictionary(ResourceResolver resourceResolver, String dictionaryPath) throws DictionaryException, ReplicationException, PersistenceException;
 
+    /**
+     * Returns the list of languages for the given dictionary resource in alphabetical order.
+     * @param dictionaryResource
+     * @return the list of languages for the given dictionary resource in alphabetical order
+     */
     List<String> getLanguages(Resource dictionaryResource);
 
     void deleteLanguage(Resource dictionaryResource, String language) throws DictionaryException, ReplicationException, PersistenceException;

core/src/main/java/be/orbinson/aem/dictionarytranslator/services/impl/CombiningMessageEntryResourceProvider.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -69,7 +70,7 @@ public class CombiningMessageEntryResourceProvider extends ResourceProvider<Obje
         Resource dictionaryResource = resourceResolver.getResource(dictionaryPath);
 
         if (dictionaryResource != null) {
-            @NotNull Map<String, Message> messagePerLanguage = new HashMap<>();
+            @NotNull Map<String, Message> messagePerLanguage = new LinkedHashMap<>(); // preserve the order of languages
             List<String> languages = dictionaryService.getLanguages(dictionaryResource);
             for (String language : languages) {
                 Message message;

core/src/test/java/be/orbinson/aem/dictionarytranslator/services/impl/CombiningMessageEntryResourceProviderTest.java
@@ -1,16 +1,25 @@
 package be.orbinson.aem.dictionarytranslator.services.impl;
 
-import com.day.cq.replication.Replicator;
-import io.wcm.testing.mock.aem.junit5.AemContext;
-import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+
+import java.util.Collection;
+import java.util.Map;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.testing.mock.sling.ResourceResolverType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.mock;
+import com.day.cq.replication.Replicator;
+
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
 
 @ExtendWith(AemContextExtension.class)
 class CombiningMessageEntryResourceProviderTest {
@@ -35,12 +44,37 @@ class CombiningMessageEntryResourceProviderTest {
     }
 
     @Test
-    void syntheticPathShouldReturnNonNullResource() {
-        assertNotNull(context.resourceResolver().getResource("/mnt/dictionary/content/dictionaries/fruit/i18n/apple"));
+    void syntheticMessageEntryPathShouldReturnNonNullResource() {
+        Resource resource = context.resourceResolver().getResource("/mnt/dictionary/content/dictionaries/fruit/i18n/apple");
+        assertNotNull(resource);
+        ValueMap properties = resource.getValueMap();
+        Map<String, Object> expectedProperties = Map.of(
+                "path", "/mnt/dictionary/content/dictionaries/fruit/i18n/apple",
+                "languages", new String[]{"en", "nl_BE"}, // languages must always be in alphabetical order
+                "nl_BE", "Appel",
+                "en", "Apple",
+                "editable", Boolean.FALSE, // because resource resolver is not adaptable to Session
+                "key", "apple",
+                "dictionaryPath", "/content/dictionaries/fruit/i18n",
+                "messageEntryPaths", new String[]{
+                        "/content/dictionaries/fruit/i18n/en/apple", "/content/dictionaries/fruit/i18n/nl_be/apple"
+                }
+        );
+        expectedProperties.forEach((key, value) -> {
+            Object actualValue = properties.get(key);
+            assertNotNull(actualValue, "Property " + key + " should not be null");
+            if (actualValue instanceof Collection) {
+                assertArrayEquals((Object[])value, ((Collection<?>)actualValue).toArray());
+            } else {
+                assertEquals(value, actualValue);
+            }
+        });
+        assertEquals(expectedProperties.size(), properties.size());
     }
 
     @Test
     void listChildrenShouldNotFailOnResource() {
         assertDoesNotThrow(() -> context.resourceResolver().getResource("/mnt/dictionary/content/dictionaries/fruit/i18n/apple").listChildren());
     }
+    
 }